3d55f3db76de9954cf0394f198c908b2b1c4ff72
Mohammad Abuelezz <aboel3zz@gmail.com>
2013-05-09 23:43:02 +0300

    add html to pages

---
+++ lirz/config/autoload/local.php
@@ -0,0 +1,36 @@
+<?php
+
+$dbParams = array(
+    'database'  => 'lirz',
+    'username'  => 'root',
+    'password'  => '123123',
+    'hostname'  => 'localhost',
+    // buffer_results - only for mysqli buffered queries, skip for others
+    'options' => array('buffer_results' => true)
+);
+
+return array(
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => function ($sm) use ($dbParams) {
+                $adapter = new BjyProfiler\Db\Adapter\ProfilingAdapter(array(
+                    'driver'    => 'pdo',
+                    'dsn'       => 'mysql:dbname='.$dbParams['database'].';host='.$dbParams['hostname'],
+                    'database'  => $dbParams['database'],
+                    'username'  => $dbParams['username'],
+                    'password'  => $dbParams['password'],
+                    'hostname'  => $dbParams['hostname'],
+                ));
+
+                $adapter->setProfiler(new BjyProfiler\Db\Profiler\Profiler);
+                if (isset($dbParams['options']) && is_array($dbParams['options'])) {
+                    $options = $dbParams['options'];
+                } else {
+                    $options = array();
+                }
+                $adapter->injectProfilingStatementPrototype($options);
+                return $adapter;
+            },
+        ),
+    ),
+);
